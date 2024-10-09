5128737217a0132c8998a25217dd9a12141b8d32
nandajfa <nanda.jfa@gmail.com>
2024-10-09 10:57:40 -0300

    feat: create new function verify token in jwt

--- src/infra/lib/jwt.ts
+++ src/infra/lib/jwt.ts
@@ -4,17 +4,34 @@ interface TokenPayload {
   uuid: string
   email: string
 }
+const secretKey = process.env.JWT_SECRET_KEY
 
 function generateToken (
   payload: TokenPayload,
   expiresIn: string = '1h'
 ): string {
-  const secretKey = process.env.JWT_SECRET_KEY
-
   if (!secretKey) {
     throw new Error('Missing JWT_SECRET_KEY environment variable')
   }
   return jwt.sign(payload, secretKey, { expiresIn })
 }
 
-export { generateToken }
+function generateActivationToken (email: string): string {
+  if (!secretKey) {
+    throw new Error('Missing JWT_SECRET_KEY environment variable')
+  }
+
+  return jwt.sign({ email }, secretKey, { expiresIn: '15min' })
+}
+
+function verifyToken (token: string): string {
+  if (!secretKey) {
+    throw new Error('Missing JWT_SECRET_KEY environment variable')
+  }
+  const payload = jwt.verify(token, secretKey) as {
+    email: string
+  }
+  return payload.email
+}
+
+export { generateToken, generateActivationToken, verifyToken }
